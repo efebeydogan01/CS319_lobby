8742196d318e2428699e9c2b596e4609867ce6f7
emirmeliherdem <melih.erdem@ug.bilkent.edu.tr>
2021-12-19 17:57:56 +0300

    added new pages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,12 +13,19 @@ import { GeneralInfoComponent } from './general-info/general-info.component';
 import { ClassInfoComponent } from './class-info/class-info.component';
 import { SocialComponent } from './social/social.component';
 import { ReportRequestComponent } from './report-request/report-request.component';
+import { GuidelinesComponent } from './guidelines/guidelines.component';
+import { WeeklyReportsComponent } from './weekly-reports/weekly-reports.component';
+import { SearchUserInfoComponent } from './search-user-info/search-user-info.component';
+import { SearchUserBarComponent } from './search-user-bar/search-user-bar.component';
 
 const appRoutes: Routes =[
   { path: '', component: LoginComponent},
   { path: 'personal-info', component: PersonalInfoComponent},
+  { path: 'search-user-info', component: SearchUserInfoComponent}, // admin & health center employee
   { path: 'class-info', component: ClassInfoComponent},
   { path: 'general-info', component: GeneralInfoComponent},
+  { path: 'general-info/guidelines', component: GuidelinesComponent},
+  { path: 'general-info/weekly-reports', component: WeeklyReportsComponent},
   { path: 'social', component: SocialComponent},
   { path: 'report-request', component: ReportRequestComponent}
 ];
@@ -33,7 +40,11 @@ const appRoutes: Routes =[
     GeneralInfoComponent,
     ClassInfoComponent,
     SocialComponent,
-    ReportRequestComponent
+    ReportRequestComponent,
+    GuidelinesComponent,
+    WeeklyReportsComponent,
+    SearchUserInfoComponent,
+    SearchUserBarComponent
   ],
   imports: [
     BrowserModule,
