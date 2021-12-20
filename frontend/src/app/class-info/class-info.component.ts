7ff6c3684a56f10d710bf92ab0eb11a19d33eda2
Efe Beydogan <efe.beydogan@ug.bilkent.edu.tr>
2021-12-20 23:43:32 +0300

    Revert "Merge branch 'frontend-emir' into frontend-efe"
    This reverts commit 5d992b0290cc2c252bb5173448dc5555cd7f955e, reversing
    changes made to 24dcb8e8c7009556d21963f984b185b78e38718f.

--- frontend/src/app/class-info/class-info.component.ts
+++ frontend/src/app/class-info/class-info.component.ts
@@ -1,37 +1,15 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {SeatComponent} from "../seat/seat.component";
-import {SeatService} from "../Services/seat.service";
-import {Subscription} from "rxjs";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-class-info',
   templateUrl: './class-info.component.html',
   styleUrls: ['./class-info.component.css']
 })
-export class ClassInfoComponent implements OnInit, OnDestroy {
-  seats: SeatComponent[] = new Array();
-  seatSubscriber: Subscription = new Subscription;
-  selectedSeat: number = -1;
-  constructor( private seatService: SeatService) { }
+export class ClassInfoComponent implements OnInit {
+
+  constructor() { }
 
   ngOnInit(): void {
-    this.seatSubscriber = this.seatService.seatSub.subscribe(
-      seat => {
-        this.seats.push( seat);
-        console.log( seat.i);
-      }
-    );
   }
 
-  ngOnDestroy() {
-    this.seatSubscriber.unsubscribe();
-  }
-
-  updateSelectedSeat(seatNo: number) {
-    if (this.selectedSeat != -1)
-      this.seats[this.selectedSeat].unselectSeat();
-
-    this.selectedSeat = seatNo;
-    console.log(this.selectedSeat);
-  }
 }
