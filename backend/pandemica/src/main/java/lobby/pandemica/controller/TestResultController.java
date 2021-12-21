9155c59d253f4e0da312a13ed886580b29b6b44b
erenpolat <63498628+erenpolat@users.noreply.github.com>
2021-12-21 22:08:33 +0300

    TestResult, Admin, MedicalEmployee added

--- backend/pandemica/src/main/java/lobby/pandemica/controller/TestResultController.java
+++ backend/pandemica/src/main/java/lobby/pandemica/controller/TestResultController.java
@@ -1,4 +1,18 @@
 package lobby.pandemica.controller;
 
-public class TestResultController {
+import lobby.pandemica.controller.base.BaseController;
+import lobby.pandemica.dto.TestResultDto;
+import lobby.pandemica.service.TestResultService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test_result")
+public class TestResultController extends BaseController<TestResultDto> {
+    private final TestResultService testResultService;
+
+    public TestResultController(TestResultService testResultService) {
+        super(testResultService);
+        this.testResultService = testResultService;
+    }
 }
