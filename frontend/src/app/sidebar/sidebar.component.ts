ecded151a25be470b1cdc030af7204f4c979e1f8
Arda Önal <arda.onal@ug.bilkent.edu.tr>
2021-12-22 19:05:47 +0300

    fixed ui improvements/bugs
    added role to personal-info html
    modified sidebar (only students, admins, academic personnel can see class-info)

--- frontend/src/app/sidebar/sidebar.component.ts
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -7,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   host: {'class': 'sidenav navbar navbar-vertical navbar-expand-xs fixed-start'}
 })
 export class SidebarComponent implements OnInit {
+  userRole: string = "";
 
   constructor() { }
 
   ngOnInit(): void {
+    this.userRole = JSON.parse( localStorage.getItem( 'userData')).role;
   }
 
 }
