3f2d6534e35844665f90f9e215724eb95c4069d0
Mert Barkın ER <mertbarkn@gmail.com>
2021-12-22 12:33:13 +0300

    Added a functionality where frontend can send a request using that user's UUID and we return that user with its role and unique attributes.

--- backend/pandemica/src/main/java/lobby/pandemica/controller/MedicalEmployeeController.java
+++ backend/pandemica/src/main/java/lobby/pandemica/controller/MedicalEmployeeController.java
@@ -2,9 +2,17 @@ package lobby.pandemica.controller;
 
 import lobby.pandemica.controller.base.BaseController;
 import lobby.pandemica.dto.MedicalEmployeeDto;
+import lobby.pandemica.dto.base.RestResponse;
 import lobby.pandemica.service.MedicalEmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("medical_employee")
@@ -14,5 +22,28 @@ public class MedicalEmployeeController extends BaseController<MedicalEmployeeDto
     public MedicalEmployeeController(MedicalEmployeeService medicalEmployeeService) {
         super(medicalEmployeeService);
         this.medicalEmployeeService = medicalEmployeeService;
+    }
+
+    @Override
+    @GetMapping("read/{id}")
+    public ResponseEntity<RestResponse<MedicalEmployeeDto>> read(@PathVariable String id)
+    {
+        try
+        {
+            return new ResponseEntity<>(new RestResponse<>(medicalEmployeeService.getUserWithRole(UUID.fromString(id)), "Get",
+                    "Getting an entity was successful."),
+                    HttpStatus.OK);
+        }
+        catch (EntityNotFoundException e)
+        {
+            return new ResponseEntity<>(new RestResponse<>(null, "Get",
+                    "Getting entity was unsuccessful due to an error with the entities given."),
+                    HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+        catch (Exception e)
+        {
+            return new ResponseEntity<>(new RestResponse<>(null, "Get","There was an unexpected error."),
+                    HttpStatus.EXPECTATION_FAILED);
+        }
     }
 }
--- backend/pandemica/src/main/java/lobby/pandemica/controller/StudentController.java
+++ backend/pandemica/src/main/java/lobby/pandemica/controller/StudentController.java
@@ -2,9 +2,17 @@ package lobby.pandemica.controller;
 
 import lobby.pandemica.controller.base.BaseController;
 import lobby.pandemica.dto.StudentDto;
+import lobby.pandemica.dto.base.RestResponse;
 import lobby.pandemica.service.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("student")
@@ -14,5 +22,28 @@ public class StudentController extends BaseController<StudentDto> {
     public StudentController(StudentService studentService) {
         super(studentService);
         this.studentService = studentService;
+    }
+
+    @Override
+    @GetMapping("read/{id}")
+    public ResponseEntity<RestResponse<StudentDto>> read(@PathVariable String id)
+    {
+        try
+        {
+            return new ResponseEntity<>(new RestResponse<>(studentService.getUserWithRole(UUID.fromString(id)), "Get",
+                    "Getting an entity was successful."),
+                    HttpStatus.OK);
+        }
+        catch (EntityNotFoundException e)
+        {
+            return new ResponseEntity<>(new RestResponse<>(null, "Get",
+                    "Getting entity was unsuccessful due to an error with the entities given."),
+                    HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+        catch (Exception e)
+        {
+            return new ResponseEntity<>(new RestResponse<>(null, "Get","There was an unexpected error."),
+                    HttpStatus.EXPECTATION_FAILED);
+        }
     }
 }
