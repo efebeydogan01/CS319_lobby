574776a237ee035dd9bf7099e7b31e20cf483dcc
erenpolat <63498628+erenpolat@users.noreply.github.com>
2021-12-22 16:13:59 +0300

    Classrooms added

---
+++ backend/pandemica/src/main/java/lobby/pandemica/db/Classrooms.java
@@ -0,0 +1,36 @@
+package lobby.pandemica.db;
+
+import lombok.*;
+
+import java.util.HashMap;
+@Data
+@Getter
+@Setter
+public class Classrooms {
+    private static HashMap<String, Boolean[][]> classrooms;
+    private final Boolean[][] b204 = {
+            {true, true, true, true, true, true, true, true, true, true},
+            {true, true, true, true, true, true, true, true, true, true},
+            {true, true, true, true, true, true, true, true, true, true},
+            {true, true, true, true, true, true, true, true, true, true},
+            {true, true, true, true, true, true, true, true, true, true}
+    };
+    private final Boolean[][] ee214 = {
+            {true, true, true, true, true, true, true, true, true, true},
+            {true, true, true, true, true, true, true, true, true, true},
+            {true, true, true, true, true, true, true, true, true, true},
+            {true, true, true, true, true, true, true, true, true, true},
+            {true, true, true, true, true, true, true, true, true, true}
+    };
+    public Classrooms(){
+        classrooms = new HashMap<>();
+        addClassroom( "B-204", b204);
+        addClassroom( "EE-214", ee214);
+    }
+    public Boolean[][] getClassroom( String classroomName){
+        return classrooms.get( classroomName);
+    }
+    public void addClassroom(String classroomName, Boolean[][] classroom){
+        classrooms.put(classroomName, classroom);
+    }
+}
