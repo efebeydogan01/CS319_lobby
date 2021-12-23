c02215e5e693cdd85c1ae7c786d972dd62f647ed
Emir Melih Erdem <melih.erdem@ug.bilkent.edu.tr>
2021-12-23 18:14:07 +0300

    added legend for seating plan

--- frontend/src/app/seat/seat.component.ts
+++ frontend/src/app/seat/seat.component.ts
@@ -9,7 +9,8 @@ import {SeatService} from "../Services/seat.service";
   styleUrls: ['./seat.component.css']
 })
 export class SeatComponent implements OnInit {
-  bntStyle: string = '';
+  seatStatus = {'no': 'bg-secondary', 'empty': 'bg-white', 'occupied': 'bg-primary', 'my': 'bg-success', 'old': 'bg-faded-success'};
+  bntStyle: string = this.seatStatus['no'];
   @Input() i: number = -1;
   @Input() exists: boolean = false;
   @Output("updateSeat") updateSeat: EventEmitter<number> = new EventEmitter<number>();
@@ -19,12 +20,12 @@ export class SeatComponent implements OnInit {
   ngOnInit(): void {
     this.seatService.emitSeat(this);
     if (this.exists)
-      this.bntStyle = 'bg-secondary';
+      this.bntStyle = this.seatStatus['empty'];
   }
 
   selectSeat() {
-    if (this.exists && this.bntStyle === 'bg-secondary') {
-      this.bntStyle = 'bg-success';
+    if (this.exists && this.bntStyle === this.seatStatus['empty']) {
+      this.bntStyle = this.seatStatus['my'];
       this.updateSeat.emit( this.i);
       console.log(this.i);
     }
@@ -32,6 +33,6 @@ export class SeatComponent implements OnInit {
 
   unselectSeat() {
     if (this.exists)
-      this.bntStyle = 'bg-secondary';
+      this.bntStyle = this.seatStatus['empty'];
   }
 }
