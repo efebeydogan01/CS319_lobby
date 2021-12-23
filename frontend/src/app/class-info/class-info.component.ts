eac7d7d9a7e0dcf7ab80488a693b0a9bd6a98c92
Emir Melih Erdem <melih.erdem@ug.bilkent.edu.tr>
2021-12-23 15:37:24 +0300

    frontend seating plan update

--- frontend/src/app/class-info/class-info.component.ts
+++ frontend/src/app/class-info/class-info.component.ts
@@ -8,30 +8,9 @@ import {Subscription} from "rxjs";
   templateUrl: './class-info.component.html',
   styleUrls: ['./class-info.component.css']
 })
-export class ClassInfoComponent implements OnInit, OnDestroy {
-  seats: SeatComponent[] = new Array();
-  seatSubscriber: Subscription = new Subscription;
-  selectedSeat: number = -1;
-  constructor( private seatService: SeatService) { }
+export class ClassInfoComponent implements OnInit {
+  constructor() { }
 
   ngOnInit(): void {
-    this.seatSubscriber = this.seatService.seatSub.subscribe(
-      seat => {
-        this.seats.push( seat);
-        console.log( seat.i);
-      }
-    );
-  }
-
-  ngOnDestroy() {
-    this.seatSubscriber.unsubscribe();
-  }
-
-  updateSelectedSeat(seatNo: number) {
-    if (this.selectedSeat != -1)
-      this.seats[this.selectedSeat].unselectSeat();
-
-    this.selectedSeat = seatNo;
-    console.log(this.selectedSeat);
   }
 }
