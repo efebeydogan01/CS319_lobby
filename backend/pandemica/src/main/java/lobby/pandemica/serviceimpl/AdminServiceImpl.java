95b6e2d14a1f761bfbcf18704f9171a09472c01f
gokberkbeydemir <gokberk.beydemir@ug.bilkent.edu.tr>
2021-12-23 14:27:05 +0300

    seating plan initialization added to admin service as a function

--- backend/pandemica/src/main/java/lobby/pandemica/serviceimpl/AdminServiceImpl.java
+++ backend/pandemica/src/main/java/lobby/pandemica/serviceimpl/AdminServiceImpl.java
@@ -1,13 +1,18 @@
 package lobby.pandemica.serviceimpl;
 
-import lobby.pandemica.db.Admin;
-import lobby.pandemica.db.User;
+import lobby.pandemica.db.*;
+import lobby.pandemica.dto.AcademicPersonnelDto;
 import lobby.pandemica.dto.AdminDto;
+import lobby.pandemica.dto.SeatDto;
+import lobby.pandemica.dto.SectionDto;
 import lobby.pandemica.repository.AdminRepository;
+import lobby.pandemica.repository.SectionRepository;
 import lobby.pandemica.repository.UserRepository;
 import lobby.pandemica.service.AdminService;
+import lobby.pandemica.service.SeatService;
 import lobby.pandemica.serviceimpl.base.BaseServiceImpl;
 import lobby.pandemica.serviceimpl.mapper.AdminMapper;
+import lobby.pandemica.serviceimpl.mapper.SectionMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -23,11 +28,16 @@ public class AdminServiceImpl extends BaseServiceImpl<Admin, AdminDto> implement
 
     private final AdminRepository adminRepository;
     private final UserRepository userRepository;
+    private final SectionRepository sectionRepository;
+    private final SeatService seatService;
 
-    public AdminServiceImpl(AdminRepository adminRepository, UserRepository userRepository) {
+    public AdminServiceImpl(AdminRepository adminRepository, UserRepository userRepository,
+                            SectionRepository sectionRepository, SeatService seatService) {
         super(adminRepository, AdminMapper.INSTANCE);
         this.adminRepository = adminRepository;
         this.userRepository = userRepository;
+        this.sectionRepository = sectionRepository;
+        this.seatService = seatService;
     }
 
     @Override
@@ -58,5 +68,32 @@ public class AdminServiceImpl extends BaseServiceImpl<Admin, AdminDto> implement
             throw new EntityNotFoundException();
         }
         return AdminMapper.INSTANCE.entityToDto(optionalAdmin.get());
+    }
+
+    public void initializeSeatingPlan(SectionDto sectionDto)
+    {
+        String courseName = sectionDto.getCourseName();
+        Integer sectionNo = sectionDto.getSectionNo();
+        if (courseName == null || sectionNo == null)
+        {
+            LOGGER.warn("Seating plan information cannot be empty!");
+            throw new EntityNotFoundException();
+        }
+        Optional<Section> infoSection = sectionRepository.findByCourseNameAndSectionNo(courseName, sectionNo);
+        if (!infoSection.isPresent())
+        {
+            LOGGER.warn("The section cannot be found!");
+            throw new EntityNotFoundException();
+        }
+
+        Classrooms classrooms = new Classrooms();
+        Boolean[][] classroom = classrooms.getClassroom(sectionDto.getClassroom());
+        int rowLength = classroom.length; int columnLength = classroom[0].length;
+        for (int i = 0; i < rowLength; i++) {
+            for (int j = 0; j < columnLength; j++) {
+                SeatDto seatDto = new SeatDto(UUID.randomUUID(), sectionDto, classroom[i][j], i, j, null);
+                seatService.create(seatDto);
+            }
+        }
     }
 }
