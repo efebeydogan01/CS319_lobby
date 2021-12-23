2294d1462f2a439c89b67d430c1d306ebd74da7f
Mert Barkın ER <mertbarkn@gmail.com>
2021-12-23 20:32:58 +0300

    Added general info.

---
+++ backend/pandemica/src/main/java/lobby/pandemica/service/AnnouncementService.java
@@ -0,0 +1,14 @@
+package lobby.pandemica.service;
+
+import lobby.pandemica.db.GeneralInfo;
+import lobby.pandemica.dto.AcademicPersonnelDto;
+import lobby.pandemica.dto.AnnouncementDto;
+import lobby.pandemica.service.base.BaseCrudService;
+
+import java.util.UUID;
+
+public interface AnnouncementService extends BaseCrudService<AnnouncementDto>
+{
+	AnnouncementDto get(UUID id);
+	GeneralInfo readGeneralInfo();
+}
