01a77c8e0ee2d44f8cda198f9d02f456bbb1cdef
erenpolat <63498628+erenpolat@users.noreply.github.com>
2021-12-23 21:42:45 +0300

    FileController and FileService added to upload files

---
+++ backend/pandemica/src/main/java/lobby/pandemica/controller/FilesController.java
@@ -0,0 +1,71 @@
+package lobby.pandemica.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import lobby.pandemica.controller.base.BaseController;
+import lobby.pandemica.db.ResponseMessage;
+import lobby.pandemica.dto.CovidInformationDto;
+import lobby.pandemica.service.FilesStorageService;
+//import org.apache.tomcat.jni.FileInfo;
+//import org.apache.tomcat.jni.FileInfo;
+import lobby.pandemica.service.base.BaseCrudService;
+import org.apache.tomcat.jni.FileInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+@RequestMapping("fileupload")
+@CrossOrigin("http://localhost:8081")
+public class FilesController extends BaseController<CovidInformationDto> {
+
+
+    @Autowired
+    FilesStorageService storageService;
+
+    public FilesController(BaseCrudService<CovidInformationDto> baseCrudService, FilesStorageService storageService) {
+        super(baseCrudService);
+        this.storageService = storageService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
+        String message = "";
+        try {
+            storageService.save(file);
+
+            message = "Uploaded the file successfully: " + file.getOriginalFilename();
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+        } catch (Exception e) {
+            message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+        }
+    }
+
+    @GetMapping("/files")
+    public ResponseEntity<List<FileInfo>> getListFiles() {
+        List<FileInfo> fileInfos = storageService.loadAll().map(path -> {
+            String filename = path.getFileName().toString();
+            String url = MvcUriComponentsBuilder.fromMethodName(FilesController.class, "getFile", path.getFileName().toString()).build().toString();
+            FileInfo fileInfo = new FileInfo();
+            fileInfo.fname = filename;
+            return fileInfo;
+        }).collect(Collectors.toList());
+
+        return ResponseEntity.status(HttpStatus.OK).body(fileInfos);
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+        Resource file = storageService.load(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+}
