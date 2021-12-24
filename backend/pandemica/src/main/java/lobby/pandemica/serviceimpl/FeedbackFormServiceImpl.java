73f7d393a0114582a267d93084ea8a1efcce64bc
Mert Barkın ER <mertbarkn@gmail.com>
2021-12-24 21:49:58 +0300

    Added Feedback forms and Request forms, fixed some stuff about long texts.

--- backend/pandemica/src/main/java/lobby/pandemica/serviceimpl/FeedbackFormServiceImpl.java
+++ backend/pandemica/src/main/java/lobby/pandemica/serviceimpl/FeedbackFormServiceImpl.java
@@ -1,15 +1,19 @@
 package lobby.pandemica.serviceimpl;
 
 import lobby.pandemica.db.FeedbackForm;
+import lobby.pandemica.db.User;
 import lobby.pandemica.dto.FeedbackFormDto;
 import lobby.pandemica.repository.FeedbackFormRepository;
+import lobby.pandemica.repository.UserRepository;
 import lobby.pandemica.repository.base.BaseRepository;
 import lobby.pandemica.service.FeedbackFormService;
 import lobby.pandemica.serviceimpl.base.BaseServiceImpl;
 import lobby.pandemica.serviceimpl.mapper.FeedbackFormMapper;
+import lobby.pandemica.serviceimpl.mapper.UserMapper;
 import lobby.pandemica.serviceimpl.mapper.base.BaseMapper;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,16 +21,27 @@ import java.util.UUID;
 public class FeedbackFormServiceImpl extends BaseServiceImpl<FeedbackForm, FeedbackFormDto> implements FeedbackFormService
 {
 	private final FeedbackFormRepository feedbackFormRepository;
-
-	public FeedbackFormServiceImpl(BaseRepository<FeedbackForm, UUID> baseRepository, BaseMapper<FeedbackForm, FeedbackFormDto> baseMapper, FeedbackFormRepository feedbackFormRepository)
+	private final UserRepository userRepository;
+	public FeedbackFormServiceImpl(BaseRepository<FeedbackForm, UUID> baseRepository,
+								   BaseMapper<FeedbackForm, FeedbackFormDto> baseMapper,
+								   FeedbackFormRepository feedbackFormRepository,
+								   UserRepository userRepository)
 	{
 		super(baseRepository, baseMapper);
 		this.feedbackFormRepository = feedbackFormRepository;
+		this.userRepository = userRepository;
 	}
 
-	@Override
 	public List<FeedbackFormDto> readAllFromUser(UUID id)
 	{
 		return FeedbackFormMapper.INSTANCE.entityListToDtoList(feedbackFormRepository.findAllByUserId(id));
+	}
+
+	@Override
+	public FeedbackFormDto create(FeedbackFormDto dto) throws EntityNotFoundException
+	{
+		User user = userRepository.getById(dto.getUser().getId());
+		dto.setUser(UserMapper.INSTANCE.entityToDto(user));
+		return super.create(dto);
 	}
 }
--- backend/pandemica/src/main/java/lobby/pandemica/serviceimpl/RequestFormServiceImpl.java
+++ backend/pandemica/src/main/java/lobby/pandemica/serviceimpl/RequestFormServiceImpl.java
@@ -1,15 +1,19 @@
 package lobby.pandemica.serviceimpl;
 
 import lobby.pandemica.db.RequestForm;
+import lobby.pandemica.db.User;
 import lobby.pandemica.dto.RequestFormDto;
 import lobby.pandemica.repository.RequestFormRepository;
+import lobby.pandemica.repository.UserRepository;
 import lobby.pandemica.repository.base.BaseRepository;
 import lobby.pandemica.service.RequestFormService;
 import lobby.pandemica.serviceimpl.base.BaseServiceImpl;
 import lobby.pandemica.serviceimpl.mapper.RequestFormMapper;
+import lobby.pandemica.serviceimpl.mapper.UserMapper;
 import lobby.pandemica.serviceimpl.mapper.base.BaseMapper;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,16 +21,26 @@ import java.util.UUID;
 public class RequestFormServiceImpl extends BaseServiceImpl<RequestForm, RequestFormDto> implements RequestFormService
 {
 	private final RequestFormRepository requestFormRepository;
+	private final UserRepository userRepository;
 
-	public RequestFormServiceImpl(BaseRepository<RequestForm, UUID> baseRepository, BaseMapper<RequestForm, RequestFormDto> baseMapper, RequestFormRepository requestFormRepository)
+	public RequestFormServiceImpl(BaseRepository<RequestForm, UUID> baseRepository, BaseMapper<RequestForm, RequestFormDto> baseMapper, RequestFormRepository requestFormRepository, UserRepository userRepository)
 	{
 		super(baseRepository, baseMapper);
 		this.requestFormRepository = requestFormRepository;
+		this.userRepository = userRepository;
 	}
 
 	@Override
 	public List<RequestFormDto> readAllFromUser(UUID id)
 	{
 		return RequestFormMapper.INSTANCE.entityListToDtoList(requestFormRepository.findAllByUserId(id));
+	}
+
+	@Override
+	public RequestFormDto create(RequestFormDto dto) throws EntityNotFoundException
+	{
+		User user = userRepository.getById(dto.getUser().getId());
+		dto.setUser(UserMapper.INSTANCE.entityToDto(user));
+		return super.create(dto);
 	}
 }
