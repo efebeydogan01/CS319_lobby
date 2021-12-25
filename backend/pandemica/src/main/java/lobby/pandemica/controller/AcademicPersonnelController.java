3141603499a15c3a872495df23ae8decbedb7f19
gokberkbeydemir <gokberk.beydemir@ug.bilkent.edu.tr>
2021-12-26 00:43:45 +0300

    academic_personnel/sections_with_seats/{useruuid}

--- backend/pandemica/src/main/java/lobby/pandemica/controller/AcademicPersonnelController.java
+++ backend/pandemica/src/main/java/lobby/pandemica/controller/AcademicPersonnelController.java
@@ -3,6 +3,7 @@ package lobby.pandemica.controller;
 import lobby.pandemica.controller.base.BaseController;
 import lobby.pandemica.db.AcademicPersonnel;
 import lobby.pandemica.db.Section;
+import lobby.pandemica.db.SectionWithSeats;
 import lobby.pandemica.dto.AcademicPersonnelDto;
 import lobby.pandemica.dto.MedicalEmployeeDto;
 import lobby.pandemica.dto.base.RestResponse;
@@ -38,6 +39,28 @@ public class AcademicPersonnelController extends BaseController<AcademicPersonne
         try
         {
             return new ResponseEntity<>(new RestResponse<>(sectionService.getSections(UUID.fromString(id)), "Get",
+                    "Getting an entity was successful."),
+                    HttpStatus.OK);
+        }
+        catch (EntityNotFoundException e)
+        {
+            return new ResponseEntity<>(new RestResponse<>(null, "Get",
+                    "Getting entity was unsuccessful due to an error with the entities given."),
+                    HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+        catch (Exception e)
+        {
+            return new ResponseEntity<>(new RestResponse<>(null, "Get","There was an unexpected error."),
+                    HttpStatus.EXPECTATION_FAILED);
+        }
+    }
+
+    @GetMapping("sections_with_seats/{id}")
+    public ResponseEntity<RestResponse<List<SectionWithSeats>>> getSectionsWithSeats(@PathVariable String id)
+    {
+        try
+        {
+            return new ResponseEntity<>(new RestResponse<>(academicPersonnelService.getSectionsWithSeats(UUID.fromString(id)), "Get",
                     "Getting an entity was successful."),
                     HttpStatus.OK);
         }
