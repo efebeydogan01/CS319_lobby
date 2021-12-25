fdd418b95a66cf96c76819c1742ebe9b7efb0c9a
Emir Melih Erdem <melih.erdem@ug.bilkent.edu.tr>
2021-12-26 01:02:47 +0300

    instructor get request for sections

--- frontend/src/app/class-info/class-info.component.ts
+++ frontend/src/app/class-info/class-info.component.ts
@@ -44,8 +44,9 @@ export class ClassInfoComponent implements OnInit {
 
   ngOnInit(): void {
     const userData = JSON.parse(localStorage.getItem(LocalStorageConstants.userData));
+    let path: string = this.userRole === 'STUDENT' ? "student/sections_with_seats/" : "academic_personnel/sections_with_seats/";
 
-    this.informationService.getSectionsWithSeats(userData.uuid).pipe( take( 1)).subscribe( {
+    this.informationService.getSectionsWithSeats(userData.uuid, path).pipe( take( 1)).subscribe( {
       next: () => {
         this.sectionsWithSeats = JSON.parse( localStorage.getItem( LocalStorageConstants.sections));
       }
